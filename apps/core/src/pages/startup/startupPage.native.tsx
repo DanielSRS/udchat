7a249124d2a1fcdfb7c070dbe9de8c52fae68e25
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2023-11-06 17:34:48 -0300

    ➕ Alias crypto with react-native-quick-crypto on mobile app

--- apps/core/src/pages/startup/startupPage.native.tsx
+++ apps/core/src/pages/startup/startupPage.native.tsx
@@ -1,9 +1,26 @@
-import { Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
+import { generateAssimetricKeys } from '../../cripto';
+import { isRight } from 'fp-ts/lib/Either';
 
 export const Startup = () => {
   return (
     <View>
       <Text>Startup page</Text>
+      <Button title={'generate keys'} onPress={async () => {
+        console.log('gerando chaves');
+        const keys = await generateAssimetricKeys();
+        console.log('terminado');
+
+        if (isRight(keys)) {
+          console.log('keys');
+          console.log(keys.right.publicKey);
+          console.log(keys.right.privateKey);
+          console.log('keus end');
+          return;
+        }
+
+        console.error('Error to generate keys');
+      }} />
     </View>
   );
 };
