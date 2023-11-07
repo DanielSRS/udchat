92ba395e4483062a62a52275941d2f2083a7b9c4
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2023-11-06 22:39:40 -0300

    ➕✨ Use startup machine on mobile startup page

--- apps/core/src/pages/startup/startupPage.native.tsx
+++ apps/core/src/pages/startup/startupPage.native.tsx
@@ -1,26 +1,53 @@
-import { Button, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import { generateAssimetricKeys } from '../../cripto';
 import { isRight } from 'fp-ts/lib/Either';
+import { useMachine } from '@xstate/react';
+import { startupMachine } from '../../machines';
 
 export const Startup = () => {
+  const [state, send] = useMachine(startupMachine, {
+    services: {
+      getUser: () => new Promise((_, r) => r()),
+      getOrg: () => new Promise((_, r) => r()),
+    }
+  });
+
+  const initialState = state.matches('findingUser');
+  const noUSer = state.matches('noUserFound');
+  const findingOrg = state.matches('findingOrg');
+  const noOrgFound = state.matches('noOrgFound');
+  const started = state.matches('started');
+
   return (
-    <View>
-      <Text>Startup page</Text>
-      <Button title={'generate keys'} onPress={async () => {
-        console.log('gerando chaves');
-        const keys = await generateAssimetricKeys();
-        console.log('terminado');
-
-        if (isRight(keys)) {
-          console.log('keys');
-          console.log(keys.right.publicKey);
-          console.log(keys.right.privateKey);
-          console.log('keus end');
-          return;
-        }
-
-        console.error('Error to generate keys');
-      }} />
+    <View style={styles.container}>
+      {!initialState ? null : (
+        <Text>Initial state</Text>
+      )}
+      {!noUSer ? null : (
+        <>
+          <Text>no user</Text>
+          <Button title={'Create user'} onPress={() => send({ type: 'CREATE_USER' })} />
+        </>
+      )}
+      {!findingOrg ? null : (
+        <Text>findingOrg</Text>
+      )}
+      {!noOrgFound ? null : (
+        <>
+          <Text>noOrgFound</Text>
+          <Button title={'Create organization'} onPress={() => send({ type: 'CREATE_ORG' })} />
+        </>
+      )}
+      {!started ? null : (
+        <Text>started</Text>
+      )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+});
