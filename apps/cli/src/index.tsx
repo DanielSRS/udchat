f5c41029d8aea0084f2572227000a653fc053374
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2023-12-10 03:05:48 -0300

    ✨ Render root view with console dimensions

--- apps/cli/src/index.tsx
+++ apps/cli/src/index.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
-import {render, Box} from 'ink';
+import React, { useEffect, useState } from 'react';
+import {render, Box, useStdout, Text} from 'ink';
 import { StartupPage } from '@udchat/core';
 
 // clear screen before using the app. This is to be able to use the hole terminal area without previos text
 const clearScreen = () => console.log("\x1b[2J\x1b[H");
 clearScreen();
 
+const useStdoutDimensions = (): [number, number] => {
+  const { stdout } = useStdout();
+  const [dimensions, setDimensions] = useState<[number, number]>([stdout.columns, stdout.rows]);
+
+  useEffect(() => {
+    const handler = () => {
+			setDimensions([stdout.columns, stdout.rows]);
+		};
+    stdout.on('resize', handler);
+    return () => {
+      stdout.off('resize', handler);
+    };
+  }, [stdout]);
+
+  return dimensions;
+}
+
 const RootView = ({ children }: { children?: React.ReactElement }) => {
+	const [c, r] = useStdoutDimensions();
 	return (
-		<Box height={36} borderStyle={'round'} borderColor={'red'}>
+		<Box height={r - 1} width={c - 1} borderStyle={'round'} borderColor={'red'} flexDirection='column'>
+			<Text>{`c: ${c} - r: ${r}`}</Text>
 			{children}
 		</Box>
 	);
