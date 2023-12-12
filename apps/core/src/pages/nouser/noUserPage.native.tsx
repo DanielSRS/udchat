4dfef58701963f31ec4ea082342794522ac8d650
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2023-12-12 07:21:52 -0300

    ✨ Add noUser page

---
+++ apps/core/src/pages/nouser/noUserPage.native.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useContextSelector } from 'use-context-selector';
+import { UserContext } from '../../contexts/user/userContext';
+import { ActivityIndicator, Button, Text, View } from 'react-native';
+
+
+export const NoUserPage = () => {
+  const findingUser = useContextSelector(UserContext, data => data.findingUser);
+  const creatingUser = useContextSelector(UserContext, data => data.creatingUser);
+  const noUserFound = useContextSelector(UserContext, data => data.noUserFound);
+  const savingFailure = useContextSelector(UserContext, data => data.savingFailure);
+  const createUser = useContextSelector(UserContext, data => data.createUser);
+
+	const createUserOptions = [
+		{
+			label: 'Sim',
+			value: true
+		},
+		{
+			label: 'Não',
+			value: false
+		},
+	];
+
+  return (
+    <View>
+      {!findingUser ? null : (
+        <Text>{`🔎 Buscando credenciais`}</Text>
+      )}
+      {!creatingUser ? null : (
+        <View>
+          <Text>Criando usuário</Text>
+          <ActivityIndicator />
+        </View>
+      )}
+      {!noUserFound ? null : (
+        <>
+          <Text>no user</Text>
+          <Button title={'Create user'} onPress={createUser} />
+        </>
+      )}
+      {!(savingFailure) ? null : (
+        <Text>{`Falha ao salvar no storage`}</Text>
+      )}
+    </View>
+  );
+}
--- apps/core/src/routes/router.tsx
+++ apps/core/src/routes/router.tsx
@@ -3,13 +3,14 @@ import { useContextSelector } from "use-context-selector"
 import { UserContext } from "../contexts/user/userContext"
 import { OrgContext } from "../contexts/organization/orgContext";
 import { StatsPage } from "../pages/stats/statsPage";
+import { NoUserPage } from "../pages/nouser/noUserPage";
 
 export const Router = () => {
   const userLoaded = useContextSelector(UserContext, data => data.userLoaded);
   const orgLoaded = useContextSelector(OrgContext, data => data.orgLoaded);
 
   if (!userLoaded) {
-    return null;
+    return <NoUserPage />;
   }
 
   if (!orgLoaded) {
